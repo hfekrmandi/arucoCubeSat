bbbbe89b2d3e93a8d4f84fd00d7eb015033d20a7
cogFrog <skyebedard@gmail.com>
2019-10-24 18:49:39 -0600

    Changes to be committed: 	modified:   arucoIdentification.py 	deleted:    charucoCalibration.py
     Untracked files:
    	arucoPoseEstimation.py
    	arucoPoseEstimationTester.py
    	calibrationData.p
    	calibrationDataCollector.py
    	calibrator.py
    
    Lots of changes that I foolishly forgot to commit back when they were made. Supdivided code into a lot of smaller pieces. For instnace, calibration image collection and calibration image processing can now be done on two seperate machines (ie: Raspberry Pi and an actual capable desktop).

--- arucoIdentification.py
+++ arucoIdentification.py
@@ -1,38 +1,51 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time, cv2
 import numpy as np
-import cv2
 import cv2.aruco as aruco
 
+shape = (1280, 960)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+parameters = aruco.DetectorParameters_create()
 
-cap = cv2.VideoCapture(0)
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = shape
+camera.framerate = 20
+rawCapture = PiRGBArray(camera, size=shape)
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    frame = frame.array
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # print(frame.shape)
-    # 480x640 is the web camera resolution
-
-    # The two lines can be interchanged to choose if the image is in grayscale or not. Grayscale saves RAM, but color makes the drawn markers easier to see.
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #gray = frame
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #frame = cv2.flip(frame, dst=None, flipCode=-1) 
     
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-    parameters =  aruco.DetectorParameters_create()
-
 
     '''    detectMarkers(...)
         detectMarkers(image, dictionary[, corners[, ids[, parameters[, rejectedI
         mgPoints]]]]) -> corners, ids, rejectedImgPoints
         '''
 
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
     print(corners)
 
-    frame = aruco.drawDetectedMarkers(gray, corners, ids, (255,255,255))
-
-    # print(rejectedImgPoints)
-    cv2.imshow('Camera View (Press \'q\' to quit)',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    frame = aruco.drawDetectedMarkers(frame, corners, ids, (255,255,255))
+    
+    # show the frame
+    cv2.imshow('Camera View (Press \'q\' to quit)', frame)
+    
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    
+    # if the `q` key was pressed, break from the loop
+    key = cv2.waitKey(1) & 0xFF
+    if (key == ord("q")):
         break
 
-cap.release()
 cv2.destroyAllWindows()
